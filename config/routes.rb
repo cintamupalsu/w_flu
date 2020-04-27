1e4ace2f447ece0828c989217ae5e9de4f11fe4c
a_maulana <cintamupalsu@hotmail.com>
2020-04-27 10:24:37 +0900

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'freeaccess_pages#home'
   get '/help', to: 'freeaccess_pages#help'
   get '/about', to: 'freeaccess_pages#about'
+  
+  get '/signup', to: 'users#new'
 end
