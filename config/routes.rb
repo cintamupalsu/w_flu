1be245c6dd4d3ac2ba0da45742448e2141a84ca0
a_maulana <cintamupalsu@hotmail.com>
2020-04-27 17:00:43 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'freeaccess_pages#home'
   get '/help', to: 'freeaccess_pages#help'
   get '/about', to: 'freeaccess_pages#about'
-  
+
   get '/signup', to: 'users#new'
+  resources :users
 end
