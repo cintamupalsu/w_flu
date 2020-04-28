ab87f6e60d2634adbd56bc0665b80f1ba374796e
a_maulana <cintamupalsu@hotmail.com>
2020-04-28 10:47:35 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
   root 'freeaccess_pages#home'
   get '/help', to: 'freeaccess_pages#help'
   get '/about', to: 'freeaccess_pages#about'
 
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
