3ea2497e487c09cf63b9c304e463e88a055c7629
a_maulana <cintamupalsu@hotmail.com>
2020-07-06 12:20:53 +0900

    Add Asyc 02

--- app/models/reportsheet.rb
+++ app/models/reportsheet.rb
@@ -6,9 +6,9 @@ class Reportsheet < ApplicationRecord
     position = Position.where("user_id=?", uid).last
     if position != nil
       (0..lats.count-1).each do |i|
-        if recdates[i] >= position.recdate
+      #  if recdates[i] >= position.recdate
           Position.create(reportsheet_id: rsid, lat: lats[i], lon: lons[i], recdate: recdates[i], user_id: uid)
-        end
+      #  end
       end
     else
       (0..lats.count-1).each do |i|
