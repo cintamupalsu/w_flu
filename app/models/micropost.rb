71e33fccafc701e6576ad646b9c2a6bebd2a5e2a
a_maulana <cintamupalsu@hotmail.com>
2020-10-09 15:38:10 +0900

    sentence 500

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 500 }
   
   def init_emotions
     # IBM Watson NLU Declaration
